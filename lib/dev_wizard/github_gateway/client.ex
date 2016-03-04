7c2e8d6e4b5086cd4d7934df3366f16c487936d3
Joel McCracken <mccracken.joel@gmail.com>
2016-03-04 15:01:24 -0500

    factorin'

---
+++ lib/dev_wizard/github_gateway/client.ex
@@ -0,0 +1,97 @@
+defmodule DevWizard.GithubGateway.Client do
+  require Logger
+  alias DevWizard.GithubGateway.Cache
+
+  defstruct(user: nil,
+            token: nil,
+            settings: nil,
+            tentacat_client: nil)
+
+  def new(gh_access_token) do
+    tentacat = Tentacat.Client.new(%{access_token: gh_access_token})
+    settings = Application.get_env(:dev_wizard, :github_settings)
+
+    user     = Tentacat.Users.me(tentacat)
+    user     = %{name: user["name"], avatar: user["avatar_url"], login: user["login"]}
+
+    %DevWizard.GithubGateway{
+      token:           gh_access_token,
+      user:            user,
+      settings:        settings,
+      tentacat_client: tentacat
+    }
+  end
+
+  def member_of_organization?(gw), do: member_of_organization?(gw, gw.user[:login])
+  def member_of_organization?(gw, username) do
+    org = gw.settings[:organization]
+
+    status =
+      Cache.fetch_or_create(
+        {:is_member, org, username},
+        60 * 10, # 10 minutes
+        fn ->
+          {req_status, _} = Tentacat.Organizations.Members.member?(org,
+                                                                   username,
+                                                                   gw.tentacat_client)
+          req_status
+        end)
+
+    case status do
+      204 -> true
+      _   -> false
+    end
+  end
+
+  def involves(gw), do: involves(gw, gw.user[:login])
+  def involves(gw, username) do
+    repos_issues_and_comments(gw, %{involving: username})
+  end
+
+  def needs_code_review(gw) do
+    repos_issues_and_comments(gw, %{labels: "Needs Code Review"})
+  end
+
+  defp repos_issues_and_comments(gw, filters) do
+    org   = gw.settings[:organization]
+    repos = gw.settings[:repositories]
+
+    Enum.reduce(repos, %{},
+      fn(repo, acc) ->
+
+        issues = Cache.fetch_or_create(
+          {:issues, repo},
+          60 * 10, # 10 minutes
+          fn ->
+            Tentacat.Issues.filter(org,
+                                   repo,
+                                   filters,
+                                   gw.tentacat_client)
+          end)
+
+        issues_with_comments = Enum.map(issues,
+          fn(issue) ->
+            comments =
+              Cache.fetch_or_create(
+                {:issue_comments, repo, issue["number"]},
+                60 * 10, # 10 minutes
+                fn ->
+                  Tentacat.Issues.Comments.list(org,
+                                                repo,
+                                                issue["number"],
+                                                gw.tentacat_client)
+                end)
+
+            Map.put(issue,
+                    "comments",
+                    comments)
+          end)
+
+        Logger.debug "GithubGateway/repo_issues_and_comments/issues_for repo: #{repo}, count: #{Enum.count issues_with_comments}, filters: #{inspect filters}"
+
+        Map.put(acc,
+                repo,
+                issues_with_comments)
+      end)
+  end
+end
--- test/models/issue_workflow_test.exs
+++ test/models/issue_workflow_test.exs
@@ -20,4 +20,18 @@ defmodule DevWizard.IssueWorkflowTest do
 
     assert Regex.run(~r/Code review assigned to dev\(s\): @carols10cents/, first_comment["body"])
   end
+
+  test "signifies if a user has signed off on this version of the PR" do
+    assigned = IssueWorkflow.pr_todo(fixture_data, "carols10cents")
+
+    apangea_assigned = assigned["apangea"]
+
+    assert Enum.count(apangea_assigned) == 1
+
+    assignment = List.first(apangea_assigned)
+    comments = assignment["comments"]
+    first_comment = List.first(comments)
+
+    assert Regex.run(~r/Code review assigned to dev\(s\): @carols10cents/, first_comment["body"])
+  end
 end
