b20789ba4e33ef50fb8e05918323699e2229a9e7
Chris Geihsler <chris@geihsler.net>
2016-03-18 16:42:57 -0400

    Displaying review status.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,11 @@
 defmodule DevWizard.PageView do
   use DevWizard.Web, :view
+
+  def review_state(issue) do
+    case issue["review_state"] do
+      :not_signed_off -> "Needs Review!"
+      :signed_off     -> "Signed Off"
+      _               -> "?"
+    end
+  end
 end
