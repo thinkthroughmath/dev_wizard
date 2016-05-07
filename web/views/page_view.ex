11f5e0d1451226212d44b81f9ff7293446708d8b
Chris Geihsler <chris@geihsler.net>
2016-05-07 08:25:18 -0400

    UI improvements for issue list.
    - Rendering milestone as a label
    - Showing more useful dates

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,11 +1,16 @@
 defmodule DevWizard.PageView do
   use DevWizard.Web, :view
+  use Timex
 
-  def review_state(issue) do
-    case issue.review_state do
-      :not_signed_off -> "Needs Review!"
-      :signed_off     -> "Signed Off"
-      _               -> "?"
+  def days_since(date) do
+    {:ok, date} = Timex.parse(date, "{ISOz}")
+
+    days = Timex.DateTime.diff(date, Timex.DateTime.now, :days)
+
+    case days do
+      0    -> "Today"
+      1    -> "Yesterday"
+      days -> "#{days} days ago"
     end
   end
 end
